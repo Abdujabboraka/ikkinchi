91956b0c5a9f245999be1c45d7f098a73b52465c
Abdujabboraka <98598866+Abdujabboraka@users.noreply.github.com>
2024-07-23 07:14:36 +0300

    Update Exercise_3.py

--- LESSON_1/Exercise_3.py
+++ LESSON_1/Exercise_3.py
@@ -1,13 +1,21 @@
 import json
 def mahsulotlar(nomi="", narxi=0.0, rangi="",soni=0):
-    try:nomi=str(input("nomi>>>"))
-    except:print("nomi matnli")
-    try:narxi =float(input("narxi>>>"))
-    except: print("narxi float")
-    try: rangi=input("rangi>>>")
-    except:print("rangi matnli")
-    try:  soni=int(input("soni>>>"))
-    except: print("soni raqamli")
+    try:
+        nomi=str(input("nomi>>>"))
+    except:
+        print("nomi matnli")
+    try:
+        narxi =float(input("narxi>>>"))
+    except: 
+        print("narxi float")
+    try: 
+        rangi=input("rangi>>>")
+    except:
+        print("rangi matnli")
+    try:  
+        soni=int(input("soni>>>"))
+    except: 
+        print("soni raqamli")
 
 
     dict_mahsulot = {"nomi":nomi,"narxi":narxi,"rangi":rangi,"soni":soni}
